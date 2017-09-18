dd3d2383e5a967472086ce657ffbb253d8bec153
dangomi <dangochan@qq.com>
2017-09-18 22:48:28 +0800

    Second

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,9 +1,31 @@
+extern crate rand;
+
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
+	let target = rand::thread_rng().gen_range(1,101);
+	println!("Target : {}",target);
+	loop{
 	println!("Input your number");
-	let mut guess = String::new();
-	io::stdin().read_line(&mut guess).expect("fail to read line");
-	println!("you guessed: {}",guess)
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess).expect("fail to read line");
+		let guess : u32 = match guess.trim().parse(){
+			Ok(num) => num,
+			Err(_) => continue
+		};
+		println!("you guessed: {}",guess);
+		match guess.cmp(&target) {
+			Ordering::Less => println!("Too small"),
+			Ordering::Greater => println!("Too big"),
+			Ordering::Equal => {
+				println!("Equal");
+				break;
+			}
+				
+		}
+	}
 }
