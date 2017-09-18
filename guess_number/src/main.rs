02b8430621c10bfc56164dae878b19de6128a1fd
dangomi <dangochan@qq.com>
2017-09-18 22:07:57 +0800

    first

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+	println!("Input your number");
+	let mut guess = String::new();
+	io::stdin().read_line(&mut guess).expect("fail to read line");
+	println!("you guessed: {}",guess)
 }
